6daae60b0971cc644e50387e2b9f2e7421d306e7
Lillian Ng <lillian.k.ng@gmail.com>
2014-02-24 01:03:32 -0800

    ch4 code

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -2,6 +2,7 @@ require "rulers/version"
 require "rulers/routing"
 require "rulers/util"
 require "rulers/dependencies"
+require "rulers/controller"
 
 module Rulers
   class Application
@@ -20,16 +21,6 @@ module Rulers
         [500, {'Content-Type' => 'text/html'},
           ["Whoops!! Internal Server Error"]]
       end
-    end
-  end
-
-  class Controller
-    def initialize(env)
-      @env = env
-    end
-
-    def env
-      @env
     end
   end
 end
