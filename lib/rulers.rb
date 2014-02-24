dc79a36e806803bce40b4940218192b3b2235523
Lillian Ng <lillian.k.ng@gmail.com>
2014-02-24 00:10:42 -0800

    ch 3 exercises

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -14,7 +14,7 @@ module Rulers
         klass, act = get_controller_and_action(env)
         controller = klass.new(env)
         text = controller.send(act)
-        puts text
+
         [200, {'Content-Type' => 'text/html'}, [text]]
       rescue
         [500, {'Content-Type' => 'text/html'},
