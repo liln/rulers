07a0b6df29a459e45190a4dbcb77809563922a04
Lillian Ng <lillian.k.ng@gmail.com>
2014-03-02 18:35:07 -0800

    ch6 code

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -16,8 +16,12 @@ module Rulers
         klass, act = get_controller_and_action(env)
         controller = klass.new(env)
         text = controller.send(act)
-
-        [200, {'Content-Type' => 'text/html'}, [text]]
+        if controller.get_response
+          st, hd, rs = controller.get_response.to_a
+          [st, hd, [rs.body].flatten]
+        else
+          [200, {'Content-Type' => 'text/html'}, [text]]
+        end
       rescue Exception => e
         error = e.inspect + "\n" + e.backtrace.join("\n")
         [500, {'Content-Type' => 'text/text'},
